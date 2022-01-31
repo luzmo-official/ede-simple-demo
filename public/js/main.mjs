bad2197db9b8234e1dfad30dd57bdbb3a9f4bb7b
TuanaCelik <tuanacelik95@gmail.com>
2022-01-31 15:05:53 +0100

    Finalized Auth layer

--- public/js/main.mjs
+++ public/js/main.mjs
@@ -14,18 +14,90 @@ export const setMode = (requestedEditMode) => {
 };
 
 // Do a request to our backend (see server.js) to retrieve an SSO key and token.
-fetch('/authorization')
-  .then(async (response) => {
-    const parsedResponse =  await response.json();
-    // We can now set the key and token to the dashboard component.
-    dashboardElement.authKey = parsedResponse.id;
-    dashboardElement.authToken = parsedResponse.token;
-    // retrieve the accessible dashboards from the Integration
-    dashboardElement.getAccessibleDashboards()
-      .then(dashboards => {
-        populateMenu(dashboards);
-      });
-  })
-  .then(() => {
-    setMode('view');
+const getDashboardAuthorizationToken = async () => {
+  // Get the platform access credentials from the current logged in user
+  const accessCredentials = await auth0.getTokenSilently();
+  //Request Cumul.io Authorization token using accessCredentials
+  const response = await fetch('/authorization', {
+    headers: new Headers({
+      Authorization: `Bearer ${accessCredentials}`,
+    }),
   });
+  const parsedResponse =  await response.json();
+  // We can now set the key and token to the dashboard component.
+  dashboardElement.authKey = parsedResponse.id;
+  dashboardElement.authToken = parsedResponse.token;
+  // retrieve the accessible dashboards from the Integration
+  const dashboards = await dashboardElement.getAccessibleDashboards();
+  populateMenu(dashboards);
+  setMode('view');
+  return parsedResponse;
+
+};
+// loads the user interface
+const initUI = async () => {
+  const isAuthenticated = await auth0.isAuthenticated(); 
+  if (isAuthenticated) {
+    document
+      .getElementById('gated-content')
+      .style.setProperty('display', 'flex', 'important');
+    getDashboardAuthorizationToken();
+  } else {
+    login();
+  }
+};
+
+// on page load
+window.onload = async () => {
+  await configureClient();
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  // If is logged in -> init UI
+  if (isAuthenticated) {
+    return initUI();
+  }
+
+  const query = window.location.search;
+  // If redirected from login
+  if (query.includes('code=') && query.includes('state=')) {
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    // Set app state based on login
+    initUI();
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, '/');
+  }
+  // If not logged in not redirected
+  else {
+    initUI();
+  }
+};
+
+/* Authentication configuration */
+let auth0 = null;
+const fetchAuthConfig = () => fetch('/auth_config.json');
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+    audience: config.audience,
+  });
+};
+
+// login function
+const login = async () => {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin,
+  });
+};
+
+document.getElementById('logout-button').onclick =  function(){logout();};
+
+// logout function
+function logout() {
+  auth0.logout({
+    returnTo: window.location.origin,
+  });
+}
--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const livereload = require('livereload');
 const connectLiveReload = require('connect-livereload');
 const compression = require('compression'); 
 const express = require('express');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const { join } = require('path');
 const Cumulio = require('cumulio');
+const authConfig = require('./auth_config.json');
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ['RS256'],
+});
 
 const client = new Cumulio({
   api_key: process.env.CUMULIO_API_KEY,
@@ -41,25 +57,35 @@ app.options('*', (req, res) => {
   res.status(204);  
 });
 
-app.get('/authorization', (req, res) => {
+app.get('/authorization',checkJwt, (req, res) => {
+  const authNamespace = 'https://cumulio/';
   client.create('authorization', {
     integration_id: process.env.INTEGRATION_ID,
     type: 'sso',
     expiry: '24 hours',
     inactivity_interval: '24 hours',
-    username: 'John Doe SSO',
-    name: 'John Doe - Integration app SSO user',
-    email: 'johndoe@cumul.io',
-    suborganization: 'Sample Integration',
-    role: 'designer',
+    role: req.user[authNamespace + 'role'],
+    name: req.user[authNamespace + 'name'],
+    username: req.user[authNamespace + 'username'],
+    email: req.user[authNamespace + 'email'],
+    suborganization: req.user[authNamespace + 'department'],
     feature_overrides: ['!flag_disable_helios']
   })
     .then(result => res.status(200).json(result))
-    .catch(error => res.status(400).json(error));
+    .catch(error =>{
+      console.log('API Error: ' + JSON.stringify(error));
+    });
 });
 
+// Endpoint to serve the configuration file
+app.get('/auth_config.json', (req, res) => {
+  res.sendFile(join(__dirname, 'auth_config.json'));
+});
+
+// Serve the index page for all other requests
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.sendFile(join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(4030, () => console.log('[OK] - App listening on port 4030'));
+
+app.listen(3000, () => console.log('[OK] - App listening on port 3000'));
